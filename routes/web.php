0a57c6712b215f0b522454ffaa84a60c0bdaf694
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-07-31 01:33:29 +0530

    Routing and Database Connection Done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test',function(){
+    return view('test',[
+        'name'=>request('name')
+    ]);
+});
+Route::get('/posts/{post}','PostController@show');
