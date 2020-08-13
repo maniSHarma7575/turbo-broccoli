59007afa735b69fdb77420dacd336d743d80b6d2
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-08-13 23:13:27 +0530

    Country Api done

---
+++ my-blog/app/Models/CountryModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountryModel extends Model
+{
+    //
+    protected $table="_z_country";
+
+    protected $guarded=[];
+}
